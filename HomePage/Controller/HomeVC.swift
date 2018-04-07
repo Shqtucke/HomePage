6bfd063978719239380680376ca92d7f93bab16f
Shaun Tucker <tucke@shauns-air.home>
2018-04-07 14:26:29 -0400

    JSON step1 added

--- HomePage/Controller/HomeVC.swift
+++ HomePage/Controller/HomeVC.swift
@@ -16,6 +16,32 @@ class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let url = Bundle.main.url(forResource: "practice", withExtension: "json")
+        
+        if let url = url {
+            let data = NSData(contentsOf: url)
+            
+            if let data = data{
+                //print(data)
+                
+                do {
+                    
+                    let jsonObject = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments)
+                    
+                    //print(jsonObject)
+                    if let object = jsonObject as? [String: AnyObject] {
+                        if let allCasinos = object["casinos"] as? [[String: AnyObject]] {
+                            print(allCasinos)
+                        }
+                    }
+                    
+                } catch {
+                    
+                    print("error occurred")
+                }
+            }
+        }
+        
 }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
